a1289ac9f1f916e0b38744d7caa970caac4dcbef
KimKyengJun <juniair.kim@dalsoft.co.kr>
2019-01-28 16:02:34 +0900

    Init Commit

---
+++ LostArkHeart.Core/Entities/Base/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostArkHeart.Core.Entities.Base
+{
+    public class EntityBase
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public DateTime? ModifiedTime { get; set; }
+    }
+}
---
+++ LostArkHeart.Core/Entities/Database.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostArkHeart.Core.Entities
+{
+    public class Database : DbContext
+    {
+
+        public override int SaveChanges()
+        {
+            UpdateTime();
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync()
+        {
+            UpdateTime();
+            return base.SaveChangesAsync();
+        }
+
+        private void UpdateTime()
+        {
+            var entities = ChangeTracker.Entries().Where(x => x.Entity is Base.EntityBase && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            foreach (var entity in entities)
+            {
+                var data = (entity.Entity as Base.EntityBase);
+                if (entity.State == EntityState.Added)
+                {
+                    data.CreatedTime = DateTime.UtcNow;
+                }
+
+                data.ModifiedTime = DateTime.UtcNow;
+            }
+        }
+    }
+}
